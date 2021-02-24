e4752938ac01a5573a7f05b9a19407574b78c405
osdanova <osdanovaonline@outlook.com>
2021-02-24 01:24:18 +0100

    Objentry update

---
+++ KH2FM_Editor/DataDictionary/ObjentryFlags.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KH2FM_Editor.DataDictionary
+{
+    public class ObjentryFlags
+    {
+        // 2 Bytes
+        public static Dictionary<ushort, String> flags = new Dictionary<ushort, string>() {
+{ 0 , "No Apdx" },
+{ 1 , "Before" },
+{ 2 , "FixColor" },
+{ 3 , "Fly" },
+{ 4 , "Scissoring" },
+{ 5 , "Pirate" },
+{ 6 , "Wall Occlusion" },
+{ 7 , "Hift" }
+        };
+
+        public static String getFlag(ushort id)
+        {
+            if (flags.ContainsKey(id)) return flags[id];
+            return "";
+        }
+
+        // 1 Byte
+        public static Dictionary<byte, String> shadowSizes = new Dictionary<byte, string>() {
+{ 0 , "None" },
+{ 1 , "Small" },
+{ 2 , "Middle" },
+{ 3 , "Large" },
+{ 4 , "Moving S" },
+{ 5 , "Moving M" },
+{ 6 , "Moving L" }
+        };
+
+        public static String getShadowSize(byte id)
+        {
+            if (shadowSizes.ContainsKey(id)) return shadowSizes[id];
+            return "";
+        }
+
+        // 1 Byte
+        public static Dictionary<byte, String> targets = new Dictionary<byte, string>() {
+{ 0 , "M" },
+{ 1 , "S" },
+{ 2 , "L" }
+        };
+
+        public static String getTarget(byte id)
+        {
+            if (targets.ContainsKey(id)) return targets[id];
+            return "";
+        }
+
+        // 1 Byte
+        public static Dictionary<byte, String> forms = new Dictionary<byte, string>() {
+{ 0 , "Default" },
+{ 6 , "Anti" },
+{ 7 , "Lion" },
+{ 8 , "Atlantica" },
+{ 9 , "Carpet" },
+{ 10 , "Roxas Dual" },
+{ 11 , "Default (Enemy)" },
+{ 12 , "Die/Card" }
+        };
+
+        public static String getForm(byte id)
+        {
+            if (forms.ContainsKey(id)) return forms[id];
+            return "";
+        }
+    }
+}
--- KH2FM_Editor/Model/Objentry/ObjentryItem.cs
+++ KH2FM_Editor/Model/Objentry/ObjentryItem.cs
@@ -3,6 +3,7 @@ using KH2FM_Editor.Libs.Utils;
 using KH2FM_Editor.Libs.Binary;
 using KH2FM_Editor.Model.COMMON;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace KH2FM_Editor.Model.Objentry
 {
@@ -10,25 +11,23 @@ namespace KH2FM_Editor.Model.Objentry
     {
         public static readonly int entrySize = 96;
         // Data Location
-        public static readonly int idOffset = 0, idSize = 2;
-        public static readonly int unk02Offset = 2, unk02Size = 2;
+        public static readonly int idOffset = 0, idSize = 4;
         public static readonly int typeOffset = 4, typeSize = 1;
-        public static readonly int unk05Offset = 5, unk05Size = 1;
-        public static readonly int unk06Offset = 6, unk06Size = 1;
+        public static readonly int subtypeOffset = 5, subtypeSize = 1;
+        public static readonly int drawPriorityOffset = 6, drawPrioritySize = 1;
         public static readonly int weaponJointOffset = 7, weaponJointSize = 1;
         public static readonly int modelNameOffset = 8, modelNameSize = 32;
         public static readonly int animNameOffset = 40, animNameSize = 32;
-        public static readonly int stateOffset = 72, stateSize = 1;
-        public static readonly int unk73Offset = 73, unk73Size = 1;
-        public static readonly int allyDmgCapOffset = 74, allyDmgCapSize = 2;
+        public static readonly int flagsOffset = 72, flagsSize = 2;
+        public static readonly int targetOffset = 74, targetSize = 1;
+        public static readonly int paddingOffset = 75, paddingSize = 1;
         public static readonly int neoStatusOffset = 76, neoStatusSize = 2;
         public static readonly int neoMovesetOffset = 78, neoMovesetSize = 2;
-        public static readonly int unk80Offset = 80, unk80Size = 2;
-        public static readonly int weightOffset = 82, weightSize = 2;
+        public static readonly int weightOffset = 80, weightSize = 4;
         public static readonly int spawnLimitOffset = 84, spawnLimitSize = 1;
-        public static readonly int unk85Offset = 85, unk85Size = 1;
-        public static readonly int unk86Offset = 86, unk86Size = 1;
-        public static readonly int comMenuOptOffset = 87, comMenuOptSize = 1;
+        public static readonly int pageOffset = 85, pageSize = 1;
+        public static readonly int shadowSizeOffset = 86, shadowSizeSize = 1;
+        public static readonly int formOffset = 87, formSize = 1;
         public static readonly int extraSpawn1Offset = 88, extraSpawn1Size = 2;
         public static readonly int extraSpawn2Offset = 90, extraSpawn2Size = 2;
         public static readonly int extraSpawn3Offset = 92, extraSpawn3Size = 2;
@@ -42,15 +41,10 @@ namespace KH2FM_Editor.Model.Objentry
         {
         }
 
-        public ushort Id
+        public uint Id
         {
-            get { return DataAccess.readUShort(raw, idOffset, idSize); }
-            set { DataAccess.writeUShort(raw, value, idOffset, idSize); }
-        }
-        public string Unk02
-        {
-            get { return DataAccess.readHexString(raw, unk02Offset, unk02Size); }
-            set { DataAccess.writeHexString(raw, value, unk02Offset, unk02Size); }
+            get { return DataAccess.readUInt(raw, idOffset, idSize); }
+            set { DataAccess.writeUInt(raw, value, idOffset, idSize); }
         }
         public string TypeValue
         {
@@ -61,15 +55,15 @@ namespace KH2FM_Editor.Model.Objentry
             get { return DataAccess.readByte(raw, typeOffset); }
             set { DataAccess.writeByte(raw, value, typeOffset); NotifyPropertyChanged(nameof(TypeValue)); }
         }
-        public string Unk05
+        public byte Subtype
         {
-            get { return DataAccess.readHexString(raw, unk05Offset, unk05Size); }
-            set { DataAccess.writeHexString(raw, value, unk05Offset, unk05Size); }
+            get { return DataAccess.readByte(raw, subtypeOffset); }
+            set { DataAccess.writeByte(raw, value, subtypeOffset); }
         }
-        public string Unk06
+        public byte DrawPriority
         {
-            get { return DataAccess.readHexString(raw, unk06Offset, unk06Size); }
-            set { DataAccess.writeHexString(raw, value, unk06Offset, unk06Size); }
+            get { return DataAccess.readByte(raw, drawPriorityOffset); }
+            set { DataAccess.writeByte(raw, value, drawPriorityOffset); }
         }
         public byte WeaponJoint
         {
@@ -90,20 +84,60 @@ namespace KH2FM_Editor.Model.Objentry
             get { return DataAccess.readString(raw, animNameOffset, animNameSize); }
             set { DataAccess.writeString32(raw, value, animNameOffset, animNameSize); }
         }
-        public byte State
+        public bool FlagNoApdx
         {
-            get { return DataAccess.readByte(raw, stateOffset); }
-            set { DataAccess.writeByte(raw, value, stateOffset); }
+            get { return new BitArray(new int[1] { Flags }).Get(0); }
         }
-        public string Unk73
+        public bool FlagBefore
         {
-            get { return DataAccess.readHexString(raw, unk73Offset, unk73Size); }
-            set { DataAccess.writeHexString(raw, value, unk73Offset, unk73Size); }
+            get { return new BitArray(new int[1] { Flags }).Get(1); }
         }
-        public string AllyDmgCap
+        public bool FlagFixColor
         {
-            get { return DataAccess.readHexString(raw, allyDmgCapOffset, allyDmgCapSize); }
-            set { DataAccess.writeHexString(raw, value, allyDmgCapOffset, allyDmgCapSize); }
+            get { return new BitArray(new int[1] { Flags }).Get(2); }
+        }
+        public bool FlagFly
+        {
+            get { return new BitArray(new int[1] { Flags }).Get(3); }
+        }
+        public bool FlagScissoring
+        {
+            get { return new BitArray(new int[1] { Flags }).Get(4); }
+        }
+        public bool FlagPirate
+        {
+            get { return new BitArray(new int[1] { Flags }).Get(5); }
+        }
+        public bool FlagWallOcclusion
+        {
+            get { return new BitArray(new int[1] { Flags }).Get(6); }
+        }
+        public bool FlagHift
+        {
+            get { return new BitArray(new int[1] { Flags }).Get(7); }
+        }
+        public ushort Flags
+        {
+            get { return DataAccess.readUShort(raw, flagsOffset, flagsSize); }
+            set { DataAccess.writeUShort(raw, value, flagsOffset, flagsSize);
+                NotifyPropertyChanged(nameof(FlagNoApdx));
+                NotifyPropertyChanged(nameof(FlagBefore));
+                NotifyPropertyChanged(nameof(FlagFixColor));
+                NotifyPropertyChanged(nameof(FlagFly));
+                NotifyPropertyChanged(nameof(FlagScissoring));
+                NotifyPropertyChanged(nameof(FlagPirate));
+                NotifyPropertyChanged(nameof(FlagWallOcclusion));
+                NotifyPropertyChanged(nameof(FlagHift));
+            }
+        }
+        public string TargetValue
+        {
+            get { return ObjentryFlags.getTarget(Target); }
+        }
+        public byte Target
+        {
+            get { return DataAccess.readByte(raw, targetOffset); }
+            set { DataAccess.writeByte(raw, value, targetOffset); }
         }
         public ushort NeoStatus
         {
@@ -115,39 +149,38 @@ namespace KH2FM_Editor.Model.Objentry
             get { return DataAccess.readUShort(raw, neoMovesetOffset, neoMovesetSize); }
             set { DataAccess.writeUShort(raw, value, neoMovesetOffset, neoMovesetSize); }
         }
-        public string Unk80
+        public float Weight
         {
-            get { return DataAccess.readHexString(raw, unk80Offset, unk80Size); }
-            set { DataAccess.writeHexString(raw, value, unk80Offset, unk80Size); }
-        }
-        public short Weight
-        {
-            get { return DataAccess.readShort(raw, weightOffset, weightSize); }
-            set { DataAccess.writeShort(raw, value, weightOffset, weightSize); }
+            get { return DataAccess.readFloat(raw, weightOffset, weightSize); }
+            set { DataAccess.writeFloat(raw, value, weightOffset, weightSize); }
         }
         public byte SpawnLimit
         {
             get { return DataAccess.readByte(raw, spawnLimitOffset); }
             set { DataAccess.writeByte(raw, value, spawnLimitOffset); }
         }
-        public string Unk85
+        public byte Page
         {
-            get { return DataAccess.readHexString(raw, unk85Offset, unk85Size); }
-            set { DataAccess.writeHexString(raw, value, unk85Offset, unk85Size); }
+            get { return DataAccess.readByte(raw, pageOffset); }
+            set { DataAccess.writeByte(raw, value, pageOffset); }
         }
-        public string Unk86
+        public string ShadowSizeValue
         {
-            get { return DataAccess.readHexString(raw, unk86Offset, unk86Size); }
-            set { DataAccess.writeHexString(raw, value, unk86Offset, unk86Size); }
+            get { return ObjentryFlags.getShadowSize(ShadowSize); }
         }
-        public string ComMenuOptValue
+        public byte ShadowSize
         {
-            get { return CommandMenu.getValue(ComMenuOpt); }
+            get { return DataAccess.readByte(raw, shadowSizeOffset); }
+            set { DataAccess.writeByte(raw, value, shadowSizeOffset); }
         }
-        public byte ComMenuOpt
+        public string FormValue
         {
-            get { return DataAccess.readByte(raw, comMenuOptOffset); }
-            set { DataAccess.writeByte(raw, value, comMenuOptOffset); NotifyPropertyChanged(nameof(ComMenuOptValue)); }
+            get { return ObjentryFlags.getForm(Form); }
+        }
+        public byte Form
+        {
+            get { return DataAccess.readByte(raw, formOffset); }
+            set { DataAccess.writeByte(raw, value, formOffset); NotifyPropertyChanged(nameof(FormValue)); }
         }
         public string ExtraSpawn1Value
         {
